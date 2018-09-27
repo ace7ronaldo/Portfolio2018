8638dc4502b97b6dba29f788836f1f0ed8f0f7e2
omijn <abhijit.kashyap07@gmail.com>
2018-09-26 18:58:01 -0700

    added typing effect on homepage using typed.js

--- index.php
+++ index.php
@@ -17,147 +17,171 @@
 		<title>Portfolio</title>
 
 		<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet"> 
-			<!--
-			CSS
-			============================================= -->
-			<link rel="stylesheet" href="css/linearicons.css">
-			<link rel="stylesheet" href="css/font-awesome.min.css">
-			<link rel="stylesheet" href="css/bootstrap.css">
-			<link rel="stylesheet" href="css/magnific-popup.css">
-			<link rel="stylesheet" href="css/nice-select.css">					
-			<link rel="stylesheet" href="css/animate.min.css">
-			<link rel="stylesheet" href="css/owl.carousel.css">
-			<link rel="stylesheet" href="css/main.css">
-		</head>
-		<body>
-			<div class="protfolio-wrap">
 
-				<!-- Start Header Area -->
-				<?php include("header.php"); ?>
-				<!-- End Header Area -->	
-				
-				<!-- start banner Area -->
-				<section class="banner-area relative" id="home">	
-					<div class="overlay overlay-bg"></div>
-					<div class="container">
-						<div class="row fullscreen d-flex align-items-center justify-content-center"> </div>
-					</div>
-				</section>
-				<!-- End banner Area -->	
-				
-				<!-- Start portfolio-area Area -->	
-				<section class="portfolio-area section-gap" id="portfolio">
-					<div class="container">
-						<div class="row d-flex justify-content-center">
-							<div class="menu-content col-lg-10">
-								<div class="title text-center">
-									<h1 class="mb-10">Portfolio</h1>
-								</div>
-							</div>
-						</div>
+		<!--CSS============================================= -->
+		<link rel="stylesheet" href="css/linearicons.css">
+		<link rel="stylesheet" href="css/font-awesome.min.css">
+		<link rel="stylesheet" href="css/bootstrap.css">
+		<link rel="stylesheet" href="css/magnific-popup.css">
+		<link rel="stylesheet" href="css/nice-select.css">					
+		<link rel="stylesheet" href="css/animate.min.css">
+		<link rel="stylesheet" href="css/owl.carousel.css">
+		<link rel="stylesheet" href="css/main.css">
 
-						<div class="filters">
-							<ul>
-								<li class="active" data-filter="*">All</li>
-								<li data-filter=".corporate">Print</li>
-								<li data-filter=".agency">UI/UX</li>
-							</ul>
-						</div>
+		<style>
+			#typedintro {
+				color: white;
+				font-size: 1.4rem;
+			}
+		</style>
+	</head>
+	<body>
+		<div class="protfolio-wrap">
 
-						<div class="filters-content">
-							<div class="row grid">
-								<div class="single-portfolio col-sm-4 all corporate">
-									<div class="item">
-										<a href="wild"> <img src="img/p1.jpg" alt="Work 1">
-											<div class="p-inner">
-												<h4>Wild</h4>
-												<div class="cat"> Book Design</div>
-											</div>
-										</a>
-									</div>
-								</div>
-								<div class="single-portfolio col-sm-4 all agency">
-									<div class="item">
-										<a href="tour" ><img src="img/p2.jpg" alt="Work 1"> 
-											<div class="p-inner">
-												<h4>Tour</h4>
-												<div class="cat"> Web VR</div>
-											</div>
-										</a>
-									</div>
-								</div>
-								<div class="single-portfolio col-sm-4 all corporate">
-									<div class="item">
-										<a href="united" ><img src="img/p3.jpg" alt="Work 1">
-											<div class="p-inner">
-												<h5>Unite to Divide then Divide to Unite</h5>
-												<div class="cat"> Poster Design</div>
-											</div>
-										</a>
-									</div>									
-								</div>
-								<div class="single-portfolio col-sm-4 all corporate">
-									<div class="item">
-										<img src="img/p4.jpg" alt="Work 1">
-										<div class="p-inner">
-											<h4>Pendulum of Chaos</h4>
-											<div class="cat"> Broadsheet Design</div>
-										</div>
-									</div>
-								</div>
-								<div class="single-portfolio col-sm-4 all agency">
-									<div class="item">
-										<img src="img/p5.jpg" alt="Work 1">
-										<div class="p-inner">
-											<h4>Why do Cells split?</h4>
-											<div class="cat">Mobile App Design</div>
-										</div>
-									</div>
-								</div>
-								<div class="single-portfolio col-sm-4 all corporate">
-									<div class="item">
-										<img src="img/p6.jpg" alt="Work 1">
-										<div class="p-inner">
-											<h5>What is Graphic Design?</h5>
-											<div class="cat"> Booklet Design</div>
-										</div>
-									</div>
-								</div>	
-								<div class="single-portfolio col-sm-4 all agency">
-									<div class="item">
-										<img src="img/p7.jpg" alt="Work 1">
-										<div class="p-inner">
-											<h5>Certainly Uncertain</h5>
-											<div class="cat">Data Visualization</div>
-										</div>
-									</div>
-								</div>							  
+			<!-- Start Header Area -->
+			<?php include("header.php"); ?>
+			<!-- End Header Area -->	
 
-							</div>
-						</div>
+			<!-- start banner Area -->
+			<section class="banner-area relative" id="home">	
+				<div class="overlay overlay-bg"></div>
+				<div class="container">
+					<div class="row fullscreen d-flex align-items-center justify-content-center">
+
+						<div id="typedintro"></div>
 
 					</div>
-				</section>
-				<!-- End portfolio-area Area -->	
+				</div>
+			</section>
+			<!-- End banner Area -->	
 
-				<?php include("footer.php"); ?>
+			<!-- Start portfolio-area Area -->	
+			<section class="portfolio-area section-gap" id="portfolio">
+				<div class="container">
+					<div class="row d-flex justify-content-center">
+						<div class="menu-content col-lg-10">
+							<div class="title text-center">
+								<h1 class="mb-10">Portfolio</h1>
+							</div>
+						</div>
+					</div>
 
-			</div>
-			<script src="js/vendor/jquery-2.2.4.min.js"></script>
-			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-			<script src="js/vendor/bootstrap.min.js"></script>			
-			<script src="js/easing.min.js"></script>			
-			<script src="js/jquery.ajaxchimp.min.js"></script>
-			<script src="js/jquery.magnific-popup.min.js"></script>	
-			<script src="js/owl.carousel.min.js"></script>			
-			<script src="js/jquery.sticky.js"></script>			
-			<script src="js/jquery.nice-select.min.js"></script>			
-			<script src="js/parallax.min.js"></script>	
-			<script src="js/mail-script.js"></script>
-			<script src="js/isotope.pkgd.min.js"></script>	
-			<script src="js/main.js"></script>	
-		</body>
-		</html>
+					<div class="filters">
+						<ul>
+							<li class="active" data-filter="*">All</li>
+							<li data-filter=".corporate">Print</li>
+							<li data-filter=".agency">UI/UX</li>
+						</ul>
+					</div>
+
+					<div class="filters-content">
+						<div class="row grid">
+							<div class="single-portfolio col-sm-4 all corporate">
+								<div class="item">
+									<a href="wild"> <img src="img/p1.jpg" alt="Work 1">
+										<div class="p-inner">
+											<h4>Wild</h4>
+											<div class="cat"> Book Design</div>
+										</div>
+									</a>
+								</div>
+							</div>
+							<div class="single-portfolio col-sm-4 all agency">
+								<div class="item">
+									<a href="tour" ><img src="img/p2.jpg" alt="Work 1"> 
+										<div class="p-inner">
+											<h4>Tour</h4>
+											<div class="cat"> Web VR</div>
+										</div>
+									</a>
+								</div>
+							</div>
+							<div class="single-portfolio col-sm-4 all corporate">
+								<div class="item">
+									<a href="united" ><img src="img/p3.jpg" alt="Work 1">
+										<div class="p-inner">
+											<h5>Unite to Divide then Divide to Unite</h5>
+											<div class="cat"> Poster Design</div>
+										</div>
+									</a>
+								</div>									
+							</div>
+							<div class="single-portfolio col-sm-4 all corporate">
+								<div class="item">
+									<img src="img/p4.jpg" alt="Work 1">
+									<div class="p-inner">
+										<h4>Pendulum of Chaos</h4>
+										<div class="cat"> Broadsheet Design</div>
+									</div>
+								</div>
+							</div>
+							<div class="single-portfolio col-sm-4 all agency">
+								<div class="item">
+									<img src="img/p5.jpg" alt="Work 1">
+									<div class="p-inner">
+										<h4>Why do Cells split?</h4>
+										<div class="cat">Mobile App Design</div>
+									</div>
+								</div>
+							</div>
+							<div class="single-portfolio col-sm-4 all corporate">
+								<div class="item">
+									<img src="img/p6.jpg" alt="Work 1">
+									<div class="p-inner">
+										<h5>What is Graphic Design?</h5>
+										<div class="cat"> Booklet Design</div>
+									</div>
+								</div>
+							</div>	
+							<div class="single-portfolio col-sm-4 all agency">
+								<div class="item">
+									<img src="img/p7.jpg" alt="Work 1">
+									<div class="p-inner">
+										<h5>Certainly Uncertain</h5>
+										<div class="cat">Data Visualization</div>
+									</div>
+								</div>
+							</div>							  
+
+						</div>
+					</div>
+
+				</div>
+			</section>
+			<!-- End portfolio-area Area -->	
+
+			<?php include("footer.php"); ?>
+
+		</div>
+		<script src="js/vendor/jquery-2.2.4.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+		<script src="js/vendor/bootstrap.min.js"></script>			
+		<script src="js/easing.min.js"></script>			
+		<script src="js/jquery.ajaxchimp.min.js"></script>
+		<script src="js/jquery.magnific-popup.min.js"></script>	
+		<script src="js/owl.carousel.min.js"></script>			
+		<script src="js/jquery.sticky.js"></script>			
+		<script src="js/jquery.nice-select.min.js"></script>			
+		<script src="js/parallax.min.js"></script>	
+		<script src="js/mail-script.js"></script>
+		<script src="js/isotope.pkgd.min.js"></script>	
+		<script src="js/main.js"></script>	
+		<script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.9"></script>
+		<script>
+			var options = {
+				strings: [
+				"mini about me thing^500",
+				"curious^500",
+				"add more sentences in the javascript^500"
+				],
+				typeSpeed: 30,				
+			}
+
+			var typed = new Typed("#typedintro", options);
+
+		</script>
+	</body>
+	</html>
 
 
 
--- united.php
+++ united.php
@@ -1,186 +1,186 @@
-	<!DOCTYPE html>
-	<html lang="zxx" class="no-js">
-	<head>
-		<!-- Mobile Specific Meta -->
-		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<!-- Favicon-->
-		<link rel="shortcut icon" href="img/fav.png">
-		<!-- Author Meta -->
-		<meta name="author" content="codepixer">
-		<!-- Meta Description -->
-		<meta name="description" content="">
-		<!-- Meta Keyword -->
-		<meta name="keywords" content="">
-		<!-- meta character set -->
-		<meta charset="UTF-8">
-		<!-- Site Title -->
-		<title>Portfolio</title>
+<!DOCTYPE html>
+<html lang="zxx" class="no-js">
+<head>
+	<!-- Mobile Specific Meta -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<!-- Favicon-->
+	<link rel="shortcut icon" href="img/fav.png">
+	<!-- Author Meta -->
+	<meta name="author" content="codepixer">
+	<!-- Meta Description -->
+	<meta name="description" content="">
+	<!-- Meta Keyword -->
+	<meta name="keywords" content="">
+	<!-- meta character set -->
+	<meta charset="UTF-8">
+	<!-- Site Title -->
+	<title>Portfolio</title>
 
-		<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet"> 
-		<!-- =================== CSS ======================== -->
-		<link rel="stylesheet" href="css/linearicons.css">
-		<link rel="stylesheet" href="css/font-awesome.min.css">
-		<link rel="stylesheet" href="css/bootstrap.css">
-		<link rel="stylesheet" href="css/magnific-popup.css">
-		<link rel="stylesheet" href="css/nice-select.css">					
-		<link rel="stylesheet" href="css/animate.min.css">
-		<link rel="stylesheet" href="css/owl.carousel.css">
-		<link rel="stylesheet" href="css/main.css">
-		<link rel="stylesheet" href ="css/gallery.css">
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-		<link href="https://fonts.googleapis.com/css?family=Droid+Sans:400,700" rel="stylesheet">
-		<link rel="stylesheet" href="https://rawgit.com/LeshikJanz/libraries/master/Bootstrap/baguetteBox.min.css">
-	</head>
-	<body>
-		<div class="protfolio-wrap">
+	<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet"> 
+	<!-- =================== CSS ======================== -->
+	<link rel="stylesheet" href="css/linearicons.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/magnific-popup.css">
+	<link rel="stylesheet" href="css/nice-select.css">					
+	<link rel="stylesheet" href="css/animate.min.css">
+	<link rel="stylesheet" href="css/owl.carousel.css">
+	<link rel="stylesheet" href="css/main.css">
+	<link rel="stylesheet" href ="css/gallery.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Droid+Sans:400,700" rel="stylesheet">
+	<link rel="stylesheet" href="https://rawgit.com/LeshikJanz/libraries/master/Bootstrap/baguetteBox.min.css">
+</head>
+<body>
+	<div class="protfolio-wrap">
 
-			<!-- Start Header Area -->
-			<?php include("header.php"); ?>
-			<!-- End Header Area -->
+		<!-- Start Header Area -->
+		<?php include("header.php"); ?>
+		<!-- End Header Area -->
 
-			
-			<!-- Start service Area -->
-			<section class="service-area section-gap relative" id="service">
-				<div class="overlay overlay-bg"></div>	
-				<div class="container">
-					<div class="row d-flex justify-content-center">
-						<div class="menu-content pb-60 col-lg-10">
-							<div class="title text-center">
-								<h1 class="mb-10 text-white">Unite to Divide, then Divide to Unite</h1>
-								<p><b>Print Media</b></p>
-							</div>
+
+		<!-- Start service Area -->
+		<section class="service-area section-gap relative" id="service">
+			<div class="overlay overlay-bg"></div>	
+			<div class="container">
+				<div class="row d-flex justify-content-center">
+					<div class="menu-content pb-60 col-lg-10">
+						<div class="title text-center">
+							<h1 class="mb-10 text-white">Unite to Divide, then Divide to Unite</h1>
+							<p><b>Print Media</b></p>
 						</div>
+					</div>
+				</div>	
+			</div>	
+		</section>
+		<!-- End service Area -->
+
+		<!-- Start services Area -->
+		<section class="services-area pb-100" >
+			<div class="container">
+				<div class="row">
+
+					<div class="col-lg-4 col-xl-12">
+						<div class="single-service">
+							<h4>With this project I was tasked with creating the beginnings of a sovereign identity system in a technological future imagined for the state of Jammu &amp; Kashmir (located in India). Throughout my explorations, I acted as a critic, using the system I developed to take on a rhetorical position.</h4>
+							<p> The viewer is taken on a journey into a possible dystopian future. The journey begins with the user a brief understanding of the history and the current situation of the state and then dive into new beginnings. As the viewer is introduced to the imagined future they are guided through each element of the sovereign identity system created and the irony of the situation. The viewer is forced acknowledge the looming question of vulnerability associated with the use of technology. </p>
+							<p>This project was published on the AndSo Graduate Journal. </p>
+							<p>This project designed using Adobe InDesign. </p>
+						</div>
+					</div>
+
+				</div>
+			</div>	
+		</section>
+		<!-- End services Area -->
+
+		<!-- Start gallery Area -->
+
+		<div class="container gallery-container">
+
+			<div class="tz-gallery">
+
+				<div class="row">
+
+					<div class="col-sm-12 col-md-4">
+						<a class="lightbox" href="img/Unite/sketch.gif">
+							<img src="img/Unite/sketch.gif" alt="sketchwork">
+						</a>
+					</div>
+					<div class="col-sm-6 col-md-4">
+						<a class="lightbox" href="img/Unite/flag.gif">
+							<img src="img/Unite/flag.gif" alt="flagwork">
+						</a>
+					</div>
+					<div class="col-sm-6 col-md-4">
+						<a class="lightbox" href="img/Unite/process.gif">
+							<img src="img/Unite/process.gif" alt="process">
+						</a>
+					</div>
+					<div class="col-sm-12 col-md-8">
+						<a class="lightbox" href="img/Unite/mathew5.jpg">
+							<img src="img/Unite/mathew5.jpg" alt="religion">
+						</a>
+					</div>
+					<div class="col-sm-6 col-md-4">
+						<a class="lightbox" href="img/Unite/mathew6.jpg">
+							<img src="img/Unite/mathew6.jpg" alt="state">
+						</a>
+					</div> 
+					<div class="col-sm-6 col-md-4">
+						<a class="lightbox" href="fimg/Unite/mathew7.jpg">
+							<img src="img/Unite/mathew7.jpg" alt="vrspace">
+						</a>
+					</div>
+					<div class="col-sm-12 col-md-4"> 
+						<a class="lightbox" href="img/Unite/mathew10.jpg">
+							<img src="img/Unite/mathew10.jpg" alt="diagram1">
+						</a>
+					</div>
+					<div class="col-sm-6 col-md-4">
+						<a class="lightbox" href="img/Unite/mathew11.jpg">
+							<img src="img/Unite/mathew11.jpg" alt="diagram2">
+						</a>
 					</div>	
-				</div>	
-			</section>
-			<!-- End service Area -->
-			
-			<!-- Start services Area -->
-			<section class="services-area pb-100" >
-				<div class="container">
-					<div class="row">
-						
-						<div class="col-lg-4 col-xl-12">
-							<div class="single-service">
-								<h4>With this project I was tasked with creating the beginnings of a sovereign identity system in a technological future imagined for the state of Jammu &amp; Kashmir (located in India). Throughout my explorations, I acted as a critic, using the system I developed to take on a rhetorical position.</h4>
-								<p> The viewer is taken on a journey into a possible dystopian future. The journey begins with the user a brief understanding of the history and the current situation of the state and then dive into new beginnings. As the viewer is introduced to the imagined future they are guided through each element of the sovereign identity system created and the irony of the situation. The viewer is forced acknowledge the looming question of vulnerability associated with the use of technology. </p>
-								<p>This project was published on the AndSo Graduate Journal. </p>
-								<p>This project designed using Adobe InDesign. </p>
-							</div>
-						</div>
-						
+					<div class="col-sm-12 col-md-4">
+						<a class="lightbox" href="img/Unite/mathew12.jpg">
+							<img src="img/Unite/mathew12.jpg" alt="diagram3">
+						</a>
 					</div>
-				</div>	
-			</section>
-			<!-- End services Area -->
-			
-			<!-- Start gallery Area -->
-			
-			<div class="container gallery-container">
-				
-				<div class="tz-gallery">
-
-					<div class="row">
-
-						<div class="col-sm-12 col-md-4">
-							<a class="lightbox" href="img/Unite/sketch.gif">
-								<img src="img/Unite/sketch.gif" alt="sketchwork">
-							</a>
-						</div>
-						<div class="col-sm-6 col-md-4">
-							<a class="lightbox" href="img/Unite/flag.gif">
-								<img src="img/Unite/flag.gif" alt="flagwork">
-							</a>
-						</div>
-						<div class="col-sm-6 col-md-4">
-							<a class="lightbox" href="img/Unite/process.gif">
-								<img src="img/Unite/process.gif" alt="process">
-							</a>
-						</div>
-						<div class="col-sm-12 col-md-8">
-							<a class="lightbox" href="img/Unite/mathew5.jpg">
-								<img src="img/Unite/mathew5.jpg" alt="religion">
-							</a>
-						</div>
-						<div class="col-sm-6 col-md-4">
-							<a class="lightbox" href="img/Unite/mathew6.jpg">
-								<img src="img/Unite/mathew6.jpg" alt="state">
-							</a>
-						</div> 
-						<div class="col-sm-6 col-md-4">
-							<a class="lightbox" href="fimg/Unite/mathew7.jpg">
-								<img src="img/Unite/mathew7.jpg" alt="vrspace">
-							</a>
-						</div>
-						<div class="col-sm-12 col-md-4"> 
-							<a class="lightbox" href="img/Unite/mathew10.jpg">
-								<img src="img/Unite/mathew10.jpg" alt="diagram1">
-							</a>
-						</div>
-						<div class="col-sm-6 col-md-4">
-							<a class="lightbox" href="img/Unite/mathew11.jpg">
-								<img src="img/Unite/mathew11.jpg" alt="diagram2">
-							</a>
-						</div>	
-						<div class="col-sm-12 col-md-4">
-							<a class="lightbox" href="img/Unite/mathew12.jpg">
-								<img src="img/Unite/mathew12.jpg" alt="diagram3">
-							</a>
-						</div>
-						<div class="col-sm-6 col-md-4">
-							<a class="lightbox" href="img/Unite/Plot 1.png">
-								<img src="img/Unite/Plot 1.png" alt="diagram1">
-							</a>
-						</div>
-						<div class="col-sm-12 col-md-4">
-							<a class="lightbox" href="img/Unite/flag.jpg">
-								<img src="img/Unite/flag.jpg" alt="flag">
-							</a>
-						</div>
-						<div class="col-sm-12 col-md-4">
-							<a class="lightbox" href="img/Unite/tattoo.jpg">
-								<img src="img/Unite/tattoo.jpg" alt="tattoo">
-							</a>
-						</div>
-						<div class="col-sm-6 col-md-4">
-							<a class="lightbox" href="img/Unite/JK-poster.jpg">
-								<img src="img/Unite/JK-poster.jpg" alt="poster">
-							</a>
-						</div>
-						<div class="col-sm-12 col-md-4">
-							<a class="lightbox" href="img/Unite/flag.jpg">
-								<img src="img/Unite/flag.jpg" alt="flag">
-							</a>
-						</div>							
-
+					<div class="col-sm-6 col-md-4">
+						<a class="lightbox" href="img/Unite/Plot 1.png">
+							<img src="img/Unite/Plot 1.png" alt="diagram1">
+						</a>
 					</div>
+					<div class="col-sm-12 col-md-4">
+						<a class="lightbox" href="img/Unite/flag.jpg">
+							<img src="img/Unite/flag.jpg" alt="flag">
+						</a>
+					</div>
+					<div class="col-sm-12 col-md-4">
+						<a class="lightbox" href="img/Unite/tattoo.jpg">
+							<img src="img/Unite/tattoo.jpg" alt="tattoo">
+						</a>
+					</div>
+					<div class="col-sm-6 col-md-4">
+						<a class="lightbox" href="img/Unite/JK-poster.jpg">
+							<img src="img/Unite/JK-poster.jpg" alt="poster">
+						</a>
+					</div>
+					<div class="col-sm-12 col-md-4">
+						<a class="lightbox" href="img/Unite/flag.jpg">
+							<img src="img/Unite/flag.jpg" alt="flag">
+						</a>
+					</div>							
 
 				</div>
 
 			</div>
-			
-			<!--end gallery area-->
 
-			<?php include("footer.php"); ?>
 		</div>
-		<script src="js/vendor/jquery-2.2.4.min.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-		<script src="js/vendor/bootstrap.min.js"></script>			
-		<script src="js/easing.min.js"></script>			
-		<script src="js/jquery.ajaxchimp.min.js"></script>
-		<script src="js/jquery.magnific-popup.min.js"></script>	
-		<script src="js/owl.carousel.min.js"></script>			
-		<script src="js/jquery.sticky.js"></script>			
-		<script src="js/jquery.nice-select.min.js"></script>			
-		<script src="js/parallax.min.js"></script>	
-		<script src="js/mail-script.js"></script>
-		<script src="js/isotope.pkgd.min.js"></script>	
-		<script src="js/main.js"></script>	
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.8.1/baguetteBox.min.js"></script>
-		<script> baguetteBox.run('.tz-gallery');</script>
-	</body>
-	</html>
+
+		<!--end gallery area-->
+
+		<?php include("footer.php"); ?>
+	</div>
+	<script src="js/vendor/jquery-2.2.4.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="js/vendor/bootstrap.min.js"></script>			
+	<script src="js/easing.min.js"></script>			
+	<script src="js/jquery.ajaxchimp.min.js"></script>
+	<script src="js/jquery.magnific-popup.min.js"></script>	
+	<script src="js/owl.carousel.min.js"></script>			
+	<script src="js/jquery.sticky.js"></script>			
+	<script src="js/jquery.nice-select.min.js"></script>			
+	<script src="js/parallax.min.js"></script>	
+	<script src="js/mail-script.js"></script>
+	<script src="js/isotope.pkgd.min.js"></script>	
+	<script src="js/main.js"></script>	
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.8.1/baguetteBox.min.js"></script>
+	<script> baguetteBox.run('.tz-gallery');</script>
+</body>
+</html>
 
 
 
